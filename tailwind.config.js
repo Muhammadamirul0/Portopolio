87e9ed1f36edc37f228110039955dec03113d65f
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2024-09-13 03:13:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      colors: {
+        primary: '#0041C2',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
