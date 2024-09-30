31bf08d1c76569e11908c3d9ed1660eaa886c11e
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2024-10-01 01:02:19 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ['Roboto Mono', 'monospace'], // Menambahkan font baru
+      },
       container: {
         center: true,
         padding: '16px',
